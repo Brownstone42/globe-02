89748b3aca10c7e3a146629f451d2489c3cf7493
Anawat <anawatbooch@gmail.com>
2025-10-23 18:44:11 +0700

    save

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>test</h1>
+</template>
+
+<script>
+export default {
+    name: 'App',
+}
+</script>
+
+<style>
+#app {
+    display: flex;
+    flex-direction: column;
+    font-family: 'Helvetica';
+}
+</style>
