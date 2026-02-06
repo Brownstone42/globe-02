b3fd3e1c274fb788c3f29c5b2553a45d992c5a69
anawatbooch <anawatbooch@gmail.com>
2026-02-06 03:48:32 +0000

    save

--- src/components/home/homeBanner.vue
+++ src/components/home/homeBanner.vue
@@ -7,19 +7,21 @@
             <source
                 type="image/webp"
                 srcset="
-                    /images/banner4.png  640w,
-                    /images/banner4.png  960w,
-                    /images/banner4.png 1280w
+                    /images/banner5.png  500w,
+                    /images/banner5.png  640w,
+                    /images/banner5.png  960w,
+                    /images/banner5.png 1280w
                 "
                 sizes="100vw"
             />
             <img
                 class="hero-img"
-                src="/images/banner4.png"
+                src="/images/banner5.png"
                 srcset="
-                    /images/banner4.png  640w,
-                    /images/banner4.png  960w,
-                    /images/banner4.png 1280w
+                    /images/banner5.png  500w,
+                    /images/banner5.png  640w,
+                    /images/banner5.png  960w,
+                    /images/banner5.png 1280w
                 "
                 sizes="100vw"
                 alt=""
